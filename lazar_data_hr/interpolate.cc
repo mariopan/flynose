3482ef4f9cb3b4094508632e51cfc606fbd905d0
mariopan <mario.pannunzi@gmail.com>
2020-05-20 11:14:09 +0100

    lazar data at high sample rate

---
+++ lazar_data_hr/interpolate.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  char buf[1024];
+  double tmp;
+  vector<double> x,y;
+  double xn,yn,sum,wght;
+  int steps, pos;
+  
+  if (argc != 6) {
+    cerr << "usage: interpolate <infile> <xmin> <xmax> <dt> <outfile>" << endl;
+    exit(1);
+  }
+
+  ifstream is(argv[1]);
+  double xmin= atof(argv[2]);
+  double xmax= atof(argv[3]);
+  double dt= atof(argv[4]);
+  if (!is.good()) {
+    cerr << "error opening input file" << endl;
+    exit(1);
+  }
+  ofstream os(argv[5]);
+  is.getline(buf, 1024, '\n');
+  is >> tmp;
+  while (is.good()) {
+    x.push_back(tmp);
+    is >>tmp;
+    y.push_back(tmp);
+    is >> tmp;
+  }
+  pos= 1;
+  steps= static_cast<int>((xmax-xmin)/dt);
+  for (int i= 0; i < steps; i++) {
+    xn= i*dt;
+    while (pos < x.size()-1 && x[pos] <= xn) pos++;
+    yn= y[pos-1]+(y[pos]-y[pos-1])/(x[pos]-x[pos-1])*(xn-x[pos-1]);
+    os << xn << " " << yn << endl;
+  }
+  os.close();
+}
+
+	
---
+++ lazar_data_hr/interpolate_them.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+./interpolate step_1.txt 0 3.5 0.0002 step_1.dat
+./interpolate step_2.txt 0 3.5 0.0002 step_2.dat
+./interpolate step_3.txt 0 3.5 0.0002 step_3.dat
+./interpolate ramp_1.txt 0 3.5 0.0002 ramp_1.dat
+./interpolate ramp_2.txt 0 3.5 0.0002 ramp_2.dat
+./interpolate ramp_3.txt 0 3.5 0.0002 ramp_3.dat
+./interpolate parabola_1.txt 0 3.5 0.0002 parabola_1.dat
+./interpolate parabola_2.txt 0 3.5 0.0002 parabola_2.dat
+./interpolate parabola_3.txt 0 3.5 0.0002 parabola_3.dat
