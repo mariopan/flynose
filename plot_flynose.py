2c68ba5134a548168691ca3cc9fb0a53b072ed6f
mariopan <mario.pannunzi@gmail.com>
2020-06-05 09:52:54 +0100

    Update plot_flynose.py
    New input to flynose.py.

--- plot_flynose.py
+++ plot_flynose.py
@@ -37,9 +37,8 @@ purple  = 'xkcd:purple'
 orange  = 'xkcd:orange'
 #cmap    = plt.get_cmap('rainbow')
 
-def martelli_plot(all_data_tmp, params2an, id_c):
+def martelli_plot(all_data_tmp, analysis_par, id_c):
     
-    t_on =0
     num_orns_glo = 40
     t = all_data_tmp[1]
     t2simulate = t[-1]
@@ -48,8 +47,10 @@ def martelli_plot(all_data_tmp, params2an, id_c):
     orn_sdf_time = all_data_tmp[4]
     
     orn2plot = np.mean(orn_sdf_norm[:,:num_orns_glo], axis=1)
-    st_name = params2an[7]
+    st_name = analysis_par[7]
     
+    
+    # SETTINGS
     if len(st_name) == 2:
         print('normalized to the peak') 
         orn2plot = orn2plot/np.max(orn2plot)
@@ -65,20 +66,20 @@ def martelli_plot(all_data_tmp, params2an, id_c):
         elif st_name == 'ramp_':
             panel_letters = ['c.', 'd.']
     
-    if id_c==0:
     
-        ax_conc_m.set_xlim(t2plot)
-        ax_orn_m.set_xlim(t2plot)
+    ax_conc_m.set_xlim(t2plot)
+    ax_orn_m.set_xlim(t2plot)
 
+    if id_c==0:
         ax_conc_m.tick_params(axis='both', labelsize=label_fs)
         ax_orn_m.tick_params(axis='both', labelsize=label_fs)
         
         ax_conc_m.set_xticklabels('')
-        ax_orn_m.set_xticklabels('')
         
         if (len(st_name) == 2) | (st_name == 'step_'):
             ax_conc_m.set_ylabel('Input ORN ', fontsize=label_fs)
             ax_orn_m.set_ylabel(r' ORN  (Hz)', fontsize=label_fs)
+            
         if len(st_name) > 2:
             ax_conc_m.set_title(st_name[:-1], fontsize = title_fs+10)
             
@@ -99,6 +100,9 @@ def martelli_plot(all_data_tmp, params2an, id_c):
             ax_conc_m.grid(True, which='both',lw=1, ls=':')
             ax_orn_m.grid(True, which='both',lw=1, ls=':')
         else:
+            ax_conc_m.set_xticks(np.linspace(0, t2simulate, 5))
+            ax_orn_m.set_xticks(np.linspace(0, t2simulate, 5))
+            
             ax_conc_m.spines['right'].set_color('none')
             ax_conc_m.spines['top'].set_color('none')
             ax_orn_m.spines['right'].set_color('none')
@@ -115,6 +119,7 @@ def martelli_plot(all_data_tmp, params2an, id_c):
         ax_orn_m.text(-.15, 1.1, panel_letters[1], transform=ax_orn_m.transAxes, color=blue,
               fontsize=panel_fs, fontweight='bold', va='top', ha='right')
             
+    # PLOT
     id_col = id_c + 3
     ax_conc_m.plot(t-t_on, 100*u_od[:,0], color=greenmap.to_rgba(id_col), linewidth=lw, 
               label='glom : '+'%d'%(1))
@@ -135,6 +140,7 @@ def orn_al_settings(ax_ornal):
         ll, bb, ww, hh = ax_ornal[id_ax].get_position().bounds
         ax_ornal[id_ax].set_position([ll+dx, bb+dy[id_ax], ww-dx, hh])
 
+    
     ax_ornal[0].set_xlim(t2plot)
     ax_ornal[1].set_xlim(t2plot)
     ax_ornal[2].set_xlim(t2plot)
@@ -176,9 +182,9 @@ def orn_al_settings(ax_ornal):
     ax_ornal[3].spines['right'].set_color('none')
     ax_ornal[3].spines['top'].set_color('none')
         
-def orn_al_plot(all_data_tmp, params2an, id_c):
+def orn_al_plot(all_data_tmp, analysis_par, id_c):
 
-    t_on =300
+    t_zero =300
     num_orns_glo = 40
     num_pns_glo = 5
     num_lns_glo = 3
@@ -196,44 +202,49 @@ def orn_al_plot(all_data_tmp, params2an, id_c):
     # ****************************************************
     # PLOTTING DATA
     id_col = id_c + 3
-    ax_ornal[0].plot(t-t_on, 100*u_od[:,0], color=greenmap.to_rgba(id_col), linewidth=lw, 
+    ax_ornal[0].plot(t-t_zero, 100*u_od[:,0], color=greenmap.to_rgba(id_col), linewidth=lw, 
                   label='glom : '+'%d'%(1))
     
-    ax_ornal[1].plot(orn_sdf_time-t_on, np.mean(orn_sdf_norm[:,:num_orns_glo], axis=1), 
+    ax_ornal[1].plot(orn_sdf_time-t_zero, np.mean(orn_sdf_norm[:,:num_orns_glo], axis=1), 
                  color=greenmap.to_rgba(id_col), linewidth=lw-1,label='sdf glo 1')
     
-    ax_ornal[2].plot(pn_sdf_time-t_on, np.mean(pn_sdf_norm[:,:num_pns_glo], axis=1), 
+    ax_ornal[2].plot(pn_sdf_time-t_zero, np.mean(pn_sdf_norm[:,:num_pns_glo], axis=1), 
                color=greenmap.to_rgba(id_col), linewidth=lw-1, label='PN, glo 1')
         
     ll = 0
-    ax_ornal[3].plot(ln_sdf_time-t_on, ln_sdf_norm[:,ll], 
+    ax_ornal[3].plot(ln_sdf_time-t_zero, ln_sdf_norm[:,ll], 
         color=greenmap.to_rgba(id_col), linewidth=lw-1, label='LN, glo 2')
     
-    if params2an[3]>0:
-        ax_ornal[0].plot(t-t_on, 100*u_od[:,1], '--',color=purplemap.to_rgba(id_col), 
+    if analysis_par[3]>0:
+        ax_ornal[0].plot(t-t_zero, 100*u_od[:,1], '--',color=purplemap.to_rgba(id_col), 
              linewidth=lw-1, label='glom : '+'%d'%(2))
-        ax_ornal[1].plot(orn_sdf_time-t_on, np.mean(orn_sdf_norm[:,num_orns_glo:], axis=1), 
+        ax_ornal[1].plot(orn_sdf_time-t_zero, np.mean(orn_sdf_norm[:,num_orns_glo:], axis=1), 
                  '--',color=purplemap.to_rgba(id_col), linewidth=lw-2,label='sdf glo 2')
-        ax_ornal[2].plot(pn_sdf_time-t_on, np.mean(pn_sdf_norm[:,num_pns_glo:], axis=1), 
+        ax_ornal[2].plot(pn_sdf_time-t_zero, np.mean(pn_sdf_norm[:,num_pns_glo:], axis=1), 
                '--', color=purplemap.to_rgba(id_col), linewidth=lw-2, label='PN, glo 2')
         ll = num_lns_glo
-        ax_ornal[3].plot(ln_sdf_time-t_on, ln_sdf_norm[:,ll], '--',
+        ax_ornal[3].plot(ln_sdf_time-t_zero, ln_sdf_norm[:,ll], '--',
                    color=purplemap.to_rgba(id_col), linewidth=lw-2, label='LN, glo 1')
     
 def olsen_orn_pn(nu_orn, sigma, nu_max):
     nu_pn = nu_max * np.power(nu_orn, 1.5)/(np.power(nu_orn, 1.5) + np.power(sigma,1.5))
     return nu_pn
        
-def olsen2010_data(all_data_tmp, params2an):
+def olsen2010_data(all_data_tmp, analysis_par):
     pts_ms  =   5
-    t_on    =   500
-    t_off   =   100+t_on+params2an[2]
+#    t_on    =   500
+    t_off   =   100+t_on+analysis_par[2]
     num_orns_glo = 40
     num_pns_glo = 5
     num_lns_glo = 5
     
     stim_on  = t_on*pts_ms 
     stim_off = t_off*pts_ms 
+#    pickle.dump([[params2an, t, u_od, 
+#     orn_sdf_norm, orn_sdf_time, 
+#     pn_sdf_norm, pn_sdf_time, 
+#     ln_sdf_norm, ln_sdf_time, 
+#     params2an_names, output_names], f)
     u_od = all_data_tmp[2]
     orn_sdf_norm = all_data_tmp[3]
     orn_sdf_time = all_data_tmp[4]
@@ -243,10 +254,6 @@ def olsen2010_data(all_data_tmp, params2an):
     ln_sdf_norm = all_data_tmp[7]
     ln_sdf_time = all_data_tmp[8]
     
-#pickle.dump([params2an, t, u_od, orn_sdf_norm, orn_sdf_time, 
-#                 pn_sdf_norm, pn_sdf_time, 
-#                 ln_sdf_norm, ln_sdf_time, 
-#                 params2an_names, output_names], f)    
     orn_id_stim = np.flatnonzero((orn_sdf_time>t_on) & (orn_sdf_time<t_off))
     pn_id_stim = np.flatnonzero((pn_sdf_time>t_on) & (pn_sdf_time<t_off))
     ln_id_stim = np.flatnonzero((ln_sdf_time>t_on) & (ln_sdf_time<t_off))
@@ -271,59 +278,61 @@ def olsen2010_data(all_data_tmp, params2an):
     
     return out_olsen
     
-#*****************************************************
-# FIG.Laz
-peaks       = [1, 2, 3]#np.linspace(0, 7, 11)
-stim_dur    = 1000  # 50 # 100 #500
-inh_conds   = ['nsi'] #['nsi', 'ln', 'noin'] #
-stim_type_tmp ='parabola_' #'ramp_' #'step_' # 
-stim_type   = stim_type_tmp
-delay2an    = 0
-peak_ratio  = 1
-b_max       = [3] # 3, 50, 150
-w_max       = [3] # 3, 50, 150
-rho         = [0] #[0, 1, 3, 5]: 
-alpha_ln    = 13.3 # ln_spike_h  = 0.4
-nsi_str     = 0.3        
-n_lines     = np.size(peaks)
-fld_analysis = 'NSI_analysis/lazar_sim/'
-
-martelli_fig= 1 # Fig.laz
-orn_al_fig  = 0
-olsen_fig   = 0
-
-fig_martelli_name   = stim_type_tmp
-fig_orn_al_name = ''
-fig_olsen_fit_name = ''
-
-fig_save    = 1
-
-
-
-
 ##*****************************************************
-## FIG: Olsen-Wilson 2010 / Martelli 2013
-#peaks       = np.linspace(0, 7, 11)
-#stim_dur    = 500  # 50 # 100 #500
-#inh_conds   = ['noin'] #['nsi', 'ln', 'noin'] #
-#stim_type   = 'ss' # 'ss'   # 'ts'
+## FIG.Laz
+#peaks       = [1, 2, 3]#np.linspace(0, 7, 11)
+#stim_dur    = 1000  # 50 # 100 #500
+#inh_conds   = ['nsi'] #['nsi', 'ln', 'noin'] #
+#stim_type_tmp = 'ramp_' #'parabola_' #'step_' # 
+#stim_type   = stim_type_tmp
 #delay2an    = 0
 #peak_ratio  = 1
-#b_max       = 3 # 3, 50, 150
-#w_max       = 3 # 3, 50, 150
-#rho         = 0 #[0, 1, 3, 5]: 
-#ln_spike_h  = 0.6
+#b_max       = [3] # 3, 50, 150
+#w_max       = [3] # 3, 50, 150
+#rho         = [0] #[0, 1, 3, 5]: 
+#alpha_ln    = 13.3 # alpha_ln  = 0.4
 #nsi_str     = 0.3        
 #n_lines     = np.size(peaks)
-#fld_analysis        = 'Olsen2010_Martelli2013/data'
-#fig_orn_al_name     = '/../images/' +'ORN-PN_Olsen2010_timecourse_dur_%d'%stim_dur
-#fig_olsen_fit_name  = '/../images/' +'ORN-PN_Olsen2010_dur_%d'%stim_dur
-#fig_martelli_name   = '/../images/' +'ORN-Martelli2013_dur_%d'%stim_dur
-#orn_al_fig      = 1 # PNORNactivity, time course side
-#olsen_fig       = 1 # PNORNactivity, Olsen side
-#martelli_fig    = 0 # ORN_response, Martelli side
-#fig_save        = 0
-#data_save       = 0
+#fld_analysis = 'NSI_analysis/lazar_sim/'
+#
+#martelli_fig= 1 # Fig.laz
+#orn_al_fig  = 0
+#olsen_fig   = 0
+#
+#fig_martelli_name   = stim_type_tmp
+#fig_orn_al_name = ''
+#fig_olsen_fit_name = ''
+#
+#fig_save    = 1
+
+
+
+
+#*****************************************************
+# FIG: Olsen-Wilson 2010 / Martelli 2013
+peaks       = np.linspace(0, 7, 11)
+stim_dur    = 100  # 50 # 100 #500
+t_on        = 300  #  500
+inh_conds   = ['noin'] #['nsi', 'ln', 'noin'] #
+stim_type   = 'ss' # 'ss'   # 'ts'
+delay2an    = 0
+peak_ratio  = 1
+b_max       = 3 # 3, 50, 150
+w_max       = 3 # 3, 50, 150
+rho         = 0 #[0, 1, 3, 5]: 
+alpha_ln    = 0.6
+nsi_str     = 0.3        
+n_lines     = np.size(peaks)
+fld_analysis        = 'Olsen2010_Martelli2013/data'
+fig_orn_al_name     = '/../images/' +'ORN-PN_Olsen2010_timecourse_dur_%d'%stim_dur
+fig_olsen_fit_name  = '/../images/' +'ORN-PN_Olsen2010_dur_%d'%stim_dur
+fig_martelli_name   = '/../images/' +'ORN-Martelli2013_dur_%d'%stim_dur
+orn_al_fig      = 0 # PNORNactivity, time course side
+olsen_fig       = 1 # PNORNactivity, Olsen side
+martelli_fig    = 0 # ORN_response, Martelli side
+
+fig_save        = 1
+data_save       = 0
 
 
 
@@ -337,7 +346,6 @@ purplemap = mpl.cm.ScalarMappable(norm=norm, cmap=mpl.cm.Purples)
 
 
 for stim_seed in [0]:
-    copyfile('flynose.py', fld_analysis+'/flynose.py') 
     
     for inh_cond in inh_conds:
         
@@ -354,7 +362,7 @@ for stim_seed in [0]:
         #******************************************************
         # FIGURE Martelli 2013
         if martelli_fig:
-            t2plot = -200, stim_dur+300
+            t2plot = -200,stim_dur+300
             if len(stim_type) > 2:
                 t2plot = 0, 3500
             rs = 2 # number of rows
@@ -378,45 +386,43 @@ for stim_seed in [0]:
             if len(stim_type)>2:
                 stim_type = stim_type_tmp+np.str(peak)
             if inh_cond == 'nsi':
-                params2an = [nsi_str, .0, stim_dur, delay2an, peak, 
+                analysis_par = [nsi_str, .0, stim_dur, delay2an, peak, 
                              peak_ratio, rho, stim_type,w_max,b_max]
             elif inh_cond == 'noin':
-                params2an = [0, 0, stim_dur, delay2an, peak, 
+                analysis_par = [0, 0, stim_dur, delay2an, peak, 
                              peak_ratio, rho, stim_type,w_max,b_max]
             elif inh_cond == 'ln':
-                params2an = [.0, ln_spike_h, stim_dur, delay2an, peak, 
+                analysis_par = [.0, alpha_ln, stim_dur, delay2an, peak, 
                              peak_ratio, rho, stim_type,w_max,b_max]
-            
-            print(params2an)
-            
+                      
             # LOAD DATA
             if len(stim_type)==2:
-                tmp_name_data = ['/ORNALrate_stim_' + params2an[7] +
-                    '_nsi_%.1f'%(params2an[0]) +
-                    '_ln_%.2f'%(params2an[1]) +
-                    '_dur2an_%d'%(params2an[2]) +
-                    '_delays2an_%d'%(params2an[3]) +
-                    '_peak_%.2f'%(params2an[4]) +
-                    '_peakratio_%.1f'%(params2an[5]) + # 
-                    '.pickle'] #'_rho_%.1f'%(params2an[6]) +  
+                tmp_name_data = ['/ORNALrate_stim_' + analysis_par[7] +
+                    '_nsi_%.1f'%(analysis_par[0]) +
+                    '_ln_%.2f'%(analysis_par[1]) +
+                    '_dur2an_%d'%(analysis_par[2]) +
+                    '_delay2an_%d'%(analysis_par[3]) +
+                    '_peak_%.1f'%(analysis_par[4]) +
+                    '_peakratio_%.1f'%(analysis_par[5]) + # 
+                    '.pickle'] #'_rho_%.1f'%(analysis_par[6]) +  
             else:
-                tmp_name_data = ['/ORNrate_stim_' + params2an[7] + 
-                                 '_nsi_%.1f'%(params2an[0]) +
-                                 '_ln_%.2f'%(params2an[1]) +
+                tmp_name_data = ['/ORNrate_stim_' + analysis_par[7] + 
+                                 '_nsi_%.1f'%(analysis_par[0]) +
+                                 '_ln_%.2f'%(analysis_par[1]) +
                                 '.pickle']
             all_name_data = tmp_name_data[0]
             
             all_data_tmp = pickle.load(open(fld_analysis+ all_name_data,  "rb" ))
 
             if orn_al_fig:
-                orn_al_plot(all_data_tmp, params2an, id_c)
+                orn_al_plot(all_data_tmp, analysis_par, id_c)
             if martelli_fig:
-                martelli_plot(all_data_tmp, params2an, id_c)
+                martelli_plot(all_data_tmp, analysis_par, id_c)
 
             if olsen_fig:
-                out_olsen = olsen2010_data(all_data_tmp, params2an)
+                out_olsen = olsen2010_data(all_data_tmp, analysis_par)
                 conc[id_c] = out_olsen[0]
-                conc_th[id_c] = params2an[4]
+                conc_th[id_c] = analysis_par[4]
                 nu_orn[id_c] = out_olsen[1]
                 nu_pn[id_c] = out_olsen[2]
                 nu_ln[id_c] = out_olsen[3]
@@ -470,9 +476,10 @@ for stim_seed in [0]:
             axs[1].set_xlabel(r'concentration [au]', fontsize=label_fs)
             axs[1].legend(loc=0, fontsize=legend_fs, frameon=False)
             
-            axs[0].text(-.2, 1.1, 'e.', transform=axs[0].transAxes, color=blue,
+            if stim_dur == 500:
+                axs[0].text(-.2, 1.1, 'e.', transform=axs[0].transAxes, color=blue,
                      fontsize=panel_fs, fontweight='bold', va='top', ha='right')
-            axs[1].text(-.2, 1.1, 'f.', transform=axs[1].transAxes,color=blue,
+                axs[1].text(-.2, 1.1, 'f.', transform=axs[1].transAxes,color=blue,
                      fontsize=panel_fs, fontweight='bold', va='top', ha='right')
         
             for j in [0,1]:
@@ -489,4 +496,3 @@ for stim_seed in [0]:
             if fig_save:
                 fig3.savefig(fld_analysis+  fig_olsen_fit_name+'_'+inh_cond+'.png')
         print('')
-
